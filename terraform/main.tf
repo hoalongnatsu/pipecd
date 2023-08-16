0095ba3107003ef3acc25e0d85fcd2e15ed5121f
hoalongnatsu <hoalongnatsu@gmail.com>
2023-08-16 14:36:04 +0700

    init

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  backend "s3" {
+    bucket         = "pipecd-s3-backend"
+    key            = "pipecd"
+    region         = "ap-southeast-1"
+    encrypt        = true
+    role_arn       = "arn:aws:iam::128937018484:role/PipecdS3BackendRole"
+    dynamodb_table = "pipecd-s3-backend"
+  }
+}
+
 provider "aws" {
   region = "ap-southeast-1"
   shared_credentials_files = ["/etc/piped-secret/credentials"]
